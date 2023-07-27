0025a466d71170127016df09c4347833a7afd44b
gwynbleiddrivia <yasinzme2@gmail.com>
2023-07-27 14:01:14 +0600

    task 2 solved

---
+++ task_2.js
@@ -0,0 +1,18 @@
+/////////////
+////Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array. 
+///Example Input: [2, -5, 10, -3, 7] Example Output: 19
+/////////////////
+
+
+function pos_sum(given){
+	let sum = 0
+	given.map(digit=>{
+		if(digit>=0){
+			sum+=digit
+		}
+	})
+	return sum
+}
+
+console.log(pos_sum([2,-5,10,-3,7]))
+console.log(pos_sum([-2,5,-10,13,7]))
